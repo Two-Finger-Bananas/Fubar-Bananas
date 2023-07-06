ada97d2c457ef90e5b4a6aebe5a5ed0c1d7662a2
mwlordmason <mason.walker1709@gmail.com>
2023-07-06 09:15:50 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
                 <Route  path="/games" element={<FetchGames setSelectedGameId={setSelectedGameId} />} />
                 <Route path="/games/:id" element={<SelectedGame selectedGameId={selectedGameId} setSelectedGameId={setSelectedGameId} />} />
                 <Route path="/games/update/:id" element={<UpdateGame />} />
+                <Route path="/games/create" element={<postGames />}
               </Routes>
             </div>
     </>
